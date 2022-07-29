702d12763196c6c9d776608ea0af3830489f9787
Burnsedia <bburnsedia@gamil.com>
2022-07-29 13:41:52 -0400

    Tweaks to the Default layout

--- frontend/src/layouts/Default.vue
+++ frontend/src/layouts/Default.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="text-gray-100">
+  <div class="text-gray-700 bg-gray-200">
 
     <!-- Header & Navigation -->
     <header class="bg-black p-6 flex flex-col md:flex-row items-center justify-between text-center">
       <strong>
         <g-link to="/" class="text-2xl">{{ $static.metadata.siteName }}</g-link>
       </strong>
-      <nav class="flex gap-x-4 text-white-800">
+      <nav class="flex gap-x-4 text-white-700">
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/blog/">Blog</g-link>
         <g-link class="nav__link" to="/services/">Services</g-link>
@@ -14,9 +14,10 @@
     </header>
 
     <!-- Page Content -->
-    <main class="p-6">
+    <main class="p-6 ">
       <slot/>
     </main>
+    <footer class='bg-black p-6 flex flex-col md:flex-row items-center justify-between text-center'> Test Test</footer>
 
   </div>
 </template>
